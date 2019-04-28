8117dfac750b59205a08a89981c55edcd8234aa1
redsberry <49972204+redsberry@users.noreply.github.com>
2019-04-29 02:52:03 +0900

    Create batch.sh

---
+++ batch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+docker stop `docker ps`
+docker rm `docker ps -a`
+docker rmi push_maria
+#docker rmi `docker images`
+rm -rf mariadb
+
+docker build -t push_maria:1.0 .
+
+# docker run -d --name dbserver -v "$(pwd)/mariadb:/var/lib/mysql" \
+# -e MYSQL_ROOT_PASSWORD=eberry \
+# -e MYSQL_DATABASE=REDSPUSH \
+# -e MYSQL_USER=redspush \
+# -e MYSQL_PASSWORD=eberry \
+# push_maria:1.0
+
+docker run -d --name dbserver -v "$(pwd)/mariadb:/var/lib/mysql" push_maria:1.0
+
+sleep 1
+
+docker ps
