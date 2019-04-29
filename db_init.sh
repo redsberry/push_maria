d45aed92f05497f34ec726fdadeb5a778aa12911
redsberry <49972204+redsberry@users.noreply.github.com>
2019-04-29 10:23:15 +0900

    Update db_init.sh

--- db_init.sh
+++ db_init.sh
@@ -3,5 +3,7 @@ echo "DB user create"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%' identified by '${MYSQL_PASSWORD}'"
 
+sleep 1
+
 echo "DB import"
 mysql -u root -p$MYSQL_ROOT_PASSWORD < /docker-entrypoint-initdb.d/dbs.import
