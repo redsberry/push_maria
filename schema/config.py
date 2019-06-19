d7e3a4400137766c2ff6afb91f2203ccbd4e5fb4
shpark <ilovefriya@gmail.com>
2019-06-19 20:19:28 +0900

    .

--- schema/config.py
+++ schema/config.py
@@ -1,18 +1,18 @@
 # MariaDB connection info.
 class Root:
-    HOST = '0.0.0.0'
+    HOST = 'localhost'
     DBNAME = 'mysql'
     USER = 'root'
     PASSWORD = 'eberry'   
-    PORT = 3306
+    PORT = 0
     CHARSET='utf8'
     AUTOCOMMIT=True
 
 class RedsPush:
-    HOST = '0.0.0.0'
+    HOST = 'localhost'
     DBNAME = 'REDSPUSH'
     USER = 'redspush'
     PASSWORD = 'eberry'   
-    PORT = 3306
+    PORT = 0
     CHARSET='utf8'
     AUTOCOMMIT=True
